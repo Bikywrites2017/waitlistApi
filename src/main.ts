97c10382a056fd3f03da39b37f5f9266146e8125
geepytechnologies <geepytechnologies@gmail.com>
2024-05-22 14:10:34 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+  const config = new DocumentBuilder()
+    .setTitle('Bikywrites')
+    .setDescription('The Bikywrites API description')
+    .setVersion('1.0')
+    .addTag('Bikywrites')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(8000);
+}
+bootstrap();
